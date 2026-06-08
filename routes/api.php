f69accc7fa8be341735c90fd95bd0aaeccf98659
Ji-Vun <rajivcandra01@students.unnes.ac.id>
2026-06-09 06:34:10 +0700

    initial commit - laravel kos project

--- routes/api.php
+++ routes/api.php
@@ -6,29 +6,14 @@ use App\Http\Controllers\Api\KamarController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RentalController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/kamar', [KamarController::class, 'index']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::get('/profile', [AuthController::class, 'profile']);
-
     Route::post('/logout', [AuthController::class, 'logout']);
-
     Route::get('/rentals', [RentalController::class, 'index']);
-
     Route::post('/rentals', [RentalController::class, 'store']);
-
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
